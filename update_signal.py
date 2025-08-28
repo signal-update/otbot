e7203469a026dc21ea64fb31d66ed1b68535368f
signal-update <rodin.idx@gmail.com>
2025-08-28 23:39:48 +0700

    upload file baru

---
+++ update_signal.py
@@ -0,0 +1,16 @@
+import os
+
+# input dari user
+target = input("Hapus file tanggal?: ")
+
+# hapus file/folder yang ada 'target' di namanya (lokal + git)
+os.system(f"rm -rf *{target}* && git rm -rf *{target}*")
+
+# commit dan push hapusannya
+os.system('git commit -m "hapus file lama"')
+os.system("git push")
+
+# upload file baru
+os.system("git add .")
+os.system('git commit -m "upload file baru"')
+os.system("git push origin main")
